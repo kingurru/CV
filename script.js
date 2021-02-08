a663adbc03c3da86f1c590f794bd83dd03d1f352
kingurru <kingnikoriginal@gmail.com>
2021-02-08 17:18:11 +0300

    fix image

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ let headerButton = document.querySelectorAll('.header-button')
 headerButton[0].addEventListener('click', function (evt) {
     evt.preventDefault()
     if (link.getAttribute('href') === 'style.css') {
-        myPhoto.setAttribute('src', './img/photo-dark.jpeg')
+        myPhoto.setAttribute('src', './img/photo-dark.jpg')
         link.setAttribute('href', 'style-dark.css')
     } else {
         link.setAttribute('href', 'style.css')
-        myPhoto.setAttribute('src', './img/photo.jpeg')
+        myPhoto.setAttribute('src', './img/photo.jpg')
     }
 })
